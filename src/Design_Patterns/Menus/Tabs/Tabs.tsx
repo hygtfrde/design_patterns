87a39cbb7eedf824592dfc95bfe570f6b8e9cc62
hygtfrde <haroldulrich@protonmail.com>
2023-02-13 20:47:27 -0800

    begin logic for Tabs

--- src/Design_Patterns/Menus/Tabs/Tabs.tsx
+++ src/Design_Patterns/Menus/Tabs/Tabs.tsx
@@ -2,6 +2,19 @@ import React from "react";
 import './tabs_styles.css';
 
 const Tabs = () => {
+    function openCity(evt, cityName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += " active";
+      }
     return (
         <div>
             <h2>Tabs</h2>
