80fbf38e3626dbeda7ba14338622661b985ec068
harold <haroldulrich@protonmail.com>
2023-03-03 11:55:50 -0800

    useState set index value on array for multiple elements and onClick exclsuion

--- src/Design_Patterns/Menus/TabHeaders/TabHeaders.tsx
+++ src/Design_Patterns/Menus/TabHeaders/TabHeaders.tsx
@@ -7,44 +7,35 @@ const TabHeaders = () => {
         length: number;
         [i: number]: any
     }
-    const [isActive, setIsActive] = useState(false);
+    const [activeIndex, setActiveIndex] = useState<string|number>('');
     
-    const openTab = (idx: string) => {
-        setIsActive(!isActive);
+    const openTab = (idx: string|number) => {
+        setActiveIndex(idx);
     }
+
+    const items = ['London', 'Paris', 'Tokyo', 'Oslo'];
+
     return (
-        <div className="container">
+        <div>
+            <p>Click on the buttons inside the tabbed menu: </p>
 
-            
-
-            <p>Click on the buttons inside the tabbed menu:</p>
-
-            <div id="London" className="tabContent">
-                <h1>London</h1>
-                <p>London is the capital city of England.</p>
+            <div className="container">
+                {items.map((item, index) => (
+                    <div
+                        key={index}
+                        className={`tabLink ${activeIndex === index ? 'tabContent' : ''}`}
+                        onClick={() => openTab(index)}
+                        id={items[index]}
+                    >
+                        {item}
+                    </div>
+                ))}
             </div>
-
-            <div id="Paris" className="tabContent">
-                <h1>Paris</h1>
-                <p>Paris is the capital of France.</p> 
-            </div>
-
-            <div id="Tokyo" className="tabContent">
-                <h1>Tokyo</h1>
-                <p>Tokyo is the capital of Japan.</p>
-            </div>
-
-            <div id="Oslo" className="tabContent">
-                <h1>Oslo</h1>
-                <p>Oslo is the capital of Norway.</p>
-            </div>
-
-            <button className={`tabLink ${isActive ? 'tabContent' : ''}`} onClick={()=>openTab('London')} id="London">London</button>
-            <button className="tabLink" onClick={()=>openTab('Paris')} id="Paris">Paris</button>
-            <button className="tabLink" onClick={()=>openTab('Tokyo')} id="Tokyo">Tokyo</button>
-            <button className="tabLink" onClick={()=>openTab('Oslo')} id="Oslo">Oslo</button>
+    
         </div>
-    )
+    );
+        
+    
 }
 
 export default TabHeaders;
