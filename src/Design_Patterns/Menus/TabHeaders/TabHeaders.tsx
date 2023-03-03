df798cf69575066db3acf9c473f2bf5624c87155
harold <haroldulrich@protonmail.com>
2023-03-02 19:23:51 -0800

    tab content

--- src/Design_Patterns/Menus/TabHeaders/TabHeaders.tsx
+++ src/Design_Patterns/Menus/TabHeaders/TabHeaders.tsx
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, {useState} from "react";
 import './tab_headers_styles.css'
 
 const TabHeaders = () => {
@@ -7,40 +7,42 @@ const TabHeaders = () => {
         length: number;
         [i: number]: any
     }
-
-    const LondonRef = useRef(null)
+    const [isActive, setIsActive] = useState(false);
     
-    const openTab = (city: string) => {
-        console.log(city)
+    const openTab = (idx: string) => {
+        setIsActive(!isActive);
     }
     return (
         <div className="container">
+
+            
+
             <p>Click on the buttons inside the tabbed menu:</p>
 
-            <div id="London" className="tabcontent">
+            <div id="London" className="tabContent">
                 <h1>London</h1>
                 <p>London is the capital city of England.</p>
             </div>
 
-            <div id="Paris" className="tabcontent">
+            <div id="Paris" className="tabContent">
                 <h1>Paris</h1>
                 <p>Paris is the capital of France.</p> 
             </div>
 
-            <div id="Tokyo" className="tabcontent">
+            <div id="Tokyo" className="tabContent">
                 <h1>Tokyo</h1>
                 <p>Tokyo is the capital of Japan.</p>
             </div>
 
-            <div id="Oslo" className="tabcontent">
+            <div id="Oslo" className="tabContent">
                 <h1>Oslo</h1>
                 <p>Oslo is the capital of Norway.</p>
             </div>
 
-            <button className="tablink" onClick={()=>openTab('London')} ref={LondonRef} id="London">London</button>
-            <button className="tablink" onClick={()=>openTab('Paris')} id="Paris">Paris</button>
-            <button className="tablink" onClick={()=>openTab('Tokyo')} id="Tokyo">Tokyo</button>
-            <button className="tablink" onClick={()=>openTab('Oslo')} id="Oslo">Oslo</button>
+            <button className={`tabLink ${isActive ? 'tabContent' : ''}`} onClick={()=>openTab('London')} id="London">London</button>
+            <button className="tabLink" onClick={()=>openTab('Paris')} id="Paris">Paris</button>
+            <button className="tabLink" onClick={()=>openTab('Tokyo')} id="Tokyo">Tokyo</button>
+            <button className="tabLink" onClick={()=>openTab('Oslo')} id="Oslo">Oslo</button>
         </div>
     )
 }
