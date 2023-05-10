c8ac3b232925fbf222e2fa2d30b3a125b9ddf7e4
harold <haroldulrich@protonmail.com>
2023-05-10 16:01:59 -0700

    set up ref outside click

--- src/Design_Patterns/Menus/SideNavigation/SideNavigation.tsx
+++ src/Design_Patterns/Menus/SideNavigation/SideNavigation.tsx
@@ -1,7 +1,26 @@
-import React, {useRef} from "react";
+import React, {useRef, useEffect, useState} from "react";
 import './side_navigation_styles.css';
 
 const SideNavigation = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const sideNav = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        // const handleSideNavClick = (event: { stopPropagation: () => void; }) => {
+        //     event.stopPropagation(); // Prevent the click event from bubbling up to the document
+        // };
+
+        // Add the event listener when the component mounts
+        document.addEventListener('click', handleClickOutside);
+        // sideNav.current && sideNav.current.addEventListener('click', handleSideNavClick);
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+          document.removeEventListener('click', handleClickOutside);
+        //   sideNav.current && sideNav.current.removeEventListener('click', handleSideNavClick);
+        };
+    }, []); // Empty dependency array ensures the effect runs only once
+    
     const vanillaOpenNav = () => {
         const mySidenav = document.getElementById("mySidenav");
         if (mySidenav !== null) {
@@ -14,26 +33,39 @@ const SideNavigation = () => {
           mySidenav.style.width = "0px";
         }    
     }
-    const sideNav = useRef<HTMLDivElement>(null);
+
+    // Function to handle clicks outside of the element
+    const handleClickOutside = (event: { stopPropagation: () => void, target: any }) => {
+        console.log('sideNav.current:', sideNav.current?.contains(event.target));
+        console.log('event.target:', event.target);
+        if (sideNav.current && !sideNav.current.contains(event.target)) {
+            if (!isOpen) {
+                console.log('!isOpen RETURN ', !isOpen)
+                return; // Early return if the sideNav is already closed
+            }
+            hotCloseNav();
+        }
+    };
     const hotOpenNav = () => {
-        if (sideNav.current !== null) {
-            sideNav.current.style.width = "250px";
-        }  
+        console.log('OPEN')
+        setIsOpen(prevState => true);
     }
     const hotCloseNav = () => {
-        if (sideNav.current !== null) {
-            sideNav.current.style.width = "0px";
-        } 
+        console.log('CLOSE')
+        setIsOpen(prevState => false);
     }
     return (
         <div>
-            <div ref={sideNav} id="mySidenav" className="sidenav">
-            <a href="javascript:void(0)" className="closebtn" onClick={()=>hotCloseNav()}>&times;</a>
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Clients</a>
-            <a href="#">Contact</a>
-            </div>
+            {
+                isOpen && 
+                <div ref={sideNav} id="mySidenav" className="sidenav">
+                    <a href="javascript:void(0)" className="closebtn" onClick={()=>hotCloseNav()}>&times;</a>
+                    <a href="#">About</a>
+                    <a href="#">Services</a>
+                    <a href="#">Clients</a>
+                    <a href="#">Contact</a>
+                </div>
+            }
 
             <h2>Animated Sidenav Example</h2>
             <p>Click on the element below to open the side navigation menu.</p>
