1e562ba8c1b8f8af8187144907788aa519537e73
harold <haroldulrich@protonmail.com>
2023-10-05 20:16:22 -0700

    init StatusBar

---
+++ src/Design_Patterns/ScrollBars/StatusBar/StatusBar.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './StatusBar.css';
+
+const StatusBar = () => {
+    useEffect(() => {
+        const updateScrollBar = () => {
+          const scrollStatus = document.querySelector(".scroll-status");
+          const scrollBar = document.querySelector(".scroll-bar") as HTMLElement;
+    
+          if (scrollStatus && scrollBar) {
+            const windowHeight = window.innerHeight;
+            const pageHeight = document.documentElement.scrollHeight - windowHeight;
+            const scrollPercentage = (window.scrollY / pageHeight) * 100;
+    
+            scrollBar.style.height = scrollPercentage + "%";
+          }
+        };
+    
+        window.addEventListener("scroll", updateScrollBar);
+    
+        return () => {
+          window.removeEventListener("scroll", updateScrollBar);
+        };
+      }, []);
+    
+
+    <div>
+        <div className="scroll-status">
+            <div className="scroll-bar"></div>
+        </div>
+    </div>
+}
+
+export default StatusBar
